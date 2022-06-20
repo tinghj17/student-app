b28000d937c43426947d4ef9d3d6f37e17e0a7a5
tinghj17 <tinghj17@gmail.com>
2022-06-20 16:21:42 -0700

    Intro props, no iteration, data in studentList

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -3,14 +3,22 @@ import "./StudentList.css";
 import Student from "./Student";
 
 const StudentList = () => {
+  const studentComponents = [
+    <li>
+      <Student name="Ada" email="ada@dev.org"></Student>
+    </li>,
+    <li>
+      <Student name="Soo-ah" email="sooah@dev.org"></Student>
+    </li>,
+    <li>
+      <Student name="Chrissy" email="chrissy@dev.org"></Student>
+    </li>,
+  ];
+
   return (
     <section>
-      <h2 className="student-list__heading"> Student List</h2>
-      <ul className="student-list">
-        <li><Student></Student></li>
-        <li><Student></Student></li>
-        <li><Student></Student></li>
-      </ul>
+      <h2>Student List</h2>
+      <ul>{studentComponents}</ul>
     </section>
   );
 };
