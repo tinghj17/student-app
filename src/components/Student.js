66be6347ec708399250b2e0d1ae7bfa27629e3b8
tinghj17 <tinghj17@gmail.com>
2022-06-20 19:32:12 -0700

    Update state in student, for change color only, no need in real data

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,12 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Student.css";
+// step 1: import
+import { useState } from "react";
 
 const Student = (props) => {
+  // step 1
+  const [isPresent, setIsPresent] = useState(false);
+  // step 3: create the event handler 
+  const togglePresence = () => {
+    setIsPresent(!isPresent);
+  }
+  // step 5: use css to change color
+  const nameColor = isPresent ? 'green':'red';
+
   return (
-    <ul>
-      <li>Nickname: {props.name}</li>
-      <li>Cool Email: {props.email}</li>
-    </ul>
+    <section>
+      <ul>
+        {/* step 5 adopt it */}
+        <li className={nameColor}>Nickname: {props.name}</li>
+        <li>Cool Email: {props.email}</li>
+      </ul>
+      {/* step 2 */}
+      {/* step 4: attach the event handler to the button */}
+      <button onClick={togglePresence}> Toggle if {props.name} is present </button>
+    </section>
   );
 };
 
